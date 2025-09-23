9ff514e00017c11dca183835af3c113ef16c6617
Advitiya Raj <144942716+rajadvitiya@users.noreply.github.com>
2025-09-23 17:12:39 +0530

    Update Api.jsx

--- src/Api.jsx
+++ src/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/api" });
+const API = axios.create({ baseURL: " https://hall-booking-backend1.onrender.com" });
 
 // Add token for admin requests
 API.interceptors.request.use((req) => {
